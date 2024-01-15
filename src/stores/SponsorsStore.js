b7a5dbd33a775d038469ba38d3e224d8dbe147a4
jomivaan <jomivaan@gmail.com>
2024-01-15 17:52:10 +0000

    Last Commit

--- src/stores/SponsorsStore.js
+++ src/stores/SponsorsStore.js
@@ -4,7 +4,6 @@ import axios from "axios";
 export const useSponsorsStore = defineStore("SponsorsStore", {
   state: () => {
     return {
-      event: null,
       jeec_api_url: process.env.VUE_APP_JEEC_BRAIN_URL,
       sponsors: {
         platinum: [],
@@ -15,11 +14,11 @@ export const useSponsorsStore = defineStore("SponsorsStore", {
     };
   },
   actions: {
-    fill() {
+    fill(eventId) {
       axios
       .get(
         process.env.VUE_APP_JEEC_WEBSITE_API_URL +
-          "/companies",
+          "/companies?event_id=" + eventId,
         {
           auth: {
             username: process.env.VUE_APP_JEEC_WEBSITE_USERNAME,
@@ -28,60 +27,6 @@ export const useSponsorsStore = defineStore("SponsorsStore", {
         }
       )
       .then((response) => (this.format(response.data["data"])));
-
-      
-
-
-      // axios
-      // .get(
-      //   process.env.VUE_APP_JEEC_WEBSITE_API_URL +
-      //     "/companies?tier=sponsor_platinum",
-      //   {
-      //     auth: {
-      //       username: process.env.VUE_APP_JEEC_WEBSITE_USERNAME,
-      //       password: process.env.VUE_APP_JEEC_WEBSITE_KEY,
-      //     },
-      //   }
-      // )
-      // .then((response) => (this.sponsors.gold = response.data["data"]));
-      // axios
-      //   .get(
-      //     process.env.VUE_APP_JEEC_WEBSITE_API_URL +
-      //       "/companies?tier=sponsor_gold",
-      //     {
-      //       auth: {
-      //         username: process.env.VUE_APP_JEEC_WEBSITE_USERNAME,
-      //         password: process.env.VUE_APP_JEEC_WEBSITE_KEY,
-      //       },
-      //     }
-      //   )
-      //   .then((response) => (this.sponsors.gold = response.data["data"]));
-
-      // axios
-      //   .get(
-      //     process.env.VUE_APP_JEEC_WEBSITE_API_URL +
-      //       "/companies?tier=sponsor_silver",
-      //     {
-      //       auth: {
-      //         username: process.env.VUE_APP_JEEC_WEBSITE_USERNAME,
-      //         password: process.env.VUE_APP_JEEC_WEBSITE_KEY,
-      //       },
-      //     }
-      //   )
-      //   .then((response) => (this.sponsors.silver = response.data["data"]));
-
-      // axios
-      //   .get(
-      //     process.env.VUE_APP_JEEC_WEBSITE_API_URL +
-      //       "/companies?tier=sponsor_bronze",
-      //     {
-      //       auth: {
-      //         username: process.env.VUE_APP_JEEC_WEBSITE_USERNAME,
-      //         password: process.env.VUE_APP_JEEC_WEBSITE_KEY,
-      //       },
-      //     }
-      //   )
-      //   .then((response) => (this.sponsors.bronze = response.data["data"]));
     },
     format(arr) {
 
@@ -90,11 +35,11 @@ export const useSponsorsStore = defineStore("SponsorsStore", {
       this.sponsors.silver = []
       this.sponsors.bronze = []
       arr.forEach((element) => {
-        this.sponsors.platinum.push(element)
-        // if(element.partnership_tier == "sponsor_platinum"){this.sponsors.platinum.push(element)}
-        // if(element.partnership_tier == "sponsor_gold"){this.sponsors.gold.push(element)}
-        // if(element.partnership_tier == "sponsor_silver"){this.sponsors.silver.push(element)}
-        // if(element.partnership_tier == "sponsor_bronze"){this.sponsors.bronze.push(element)}
+        // this.sponsors.platinum.push(element)
+        if(element.partnership_tier == "sponsor_platinum"){this.sponsors.platinum.push(element)}
+        if(element.partnership_tier == "sponsor_gold"){this.sponsors.platinum.push(element)}
+        if(element.partnership_tier == "sponsor_silver"){this.sponsors.platinum.push(element)}
+        if(element.partnership_tier == "sponsor_bronze"){this.sponsors.platinum.push(element)}
 
       });
     },
