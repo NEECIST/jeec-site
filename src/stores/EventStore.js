09ae0f735ec45d76fb7bf3f6ae8c853c48c73315
D4MPPT <daniel.pacheco@tecnico.ulisboa.pt>
2024-02-17 01:57:53 +0000

    stopped autistic behavior of buttons

--- src/stores/EventStore.js
+++ src/stores/EventStore.js
@@ -26,7 +26,8 @@ export const useEventStore = defineStore("EventStore", {
           },
         })
         .then((response) => {
-          this.event_default_id = response.data.event_id
+          this.event_default_id = response.data.event_id;
+          this.event_id = response.data.event_id;
         });
 
       axios
