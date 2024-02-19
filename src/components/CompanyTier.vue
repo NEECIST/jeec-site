58eadfb40ce656898aabd860e3d8050cd6e9a8ff
deco <andrefsantos@skiff.com>
2024-02-19 00:30:40 +0000

    added expand option on companies carousel

--- src/components/CompanyTier.vue
+++ src/components/CompanyTier.vue
@@ -5,38 +5,50 @@
         <svg>
           <defs>
             <mask :id="'companies__mask__' + tier" x="0" y="0" width="100%" height="100%">
-              <rect fill="white" x="0" y="0" width="100%" height="100%"/>
+              <rect fill="white" x="0" y="0" width="100%" height="100%" />
               <text class="mask__text" x="50%" y="50%">{{ tier }}</text>
             </mask>
           </defs>
-          
-          <rect class="mask__base" :mask="'url(#companies__mask__' + tier + ')'" x="0" y="0" width="100%" height="105%"/>
-        
+
+          <rect class="mask__base" :mask="'url(#companies__mask__' + tier + ')'" x="0" y="0" width="100%" height="105%" />
+
         </svg>
         <div :id="'tier__text__background-' + tier" class="tier__text__background"></div>
         <h2>{{ tier }}</h2>
       </div>
       <h2>{{ type }}</h2>
     </div>
-    <div v-if="companies.length > 1" class="carousel__border">
+    <div class="carousel__border" :class="{ expanded: expanded }">
       <div class="carousel__wrapper">
-        <carousel :breakpoints="carousel_breakpoints" :autoplay="2000" :wrap-around="true" :transition="500">
-          <slide v-for="company in companies" :key="company">
-            <a target="_blank" :href="company.link ? company.link : 'jeec.ist'" class="carousel__item">
-              <img class="company__logo" :src="jeec_api_url + company.logo" :alt="company.name">
-            </a>
-          </slide>
-        </carousel>
+        <Transition  name="fade-grow" mode="out-in">
+          <div v-if="expanded" class="expanded-wrapper">
+            <slide v-for="company in companies">
+              <a target="_blank" :href="company.link ? company.link : 'jeec.ist'" class="carousel__item">
+                <img class="company__logo" :src="jeec_api_url + company.logo" :alt="company.name">
+              </a>
+            </slide>
+          </div>
+          <div v-else>
+            <carousel v-if="companies.length > 1" :breakpoints="carousel_breakpoints" :autoplay="2000" :wrap-around="true"
+              :transition="500">
+              <slide v-for="company in companies" :key="company">
+                <a target="_blank" :href="company.link ? company.link : 'jeec.ist'" class="carousel__item">
+                  <img class="company__logo" :src="jeec_api_url + company.logo" :alt="company.name">
+                </a>
+              </slide>
+            </carousel>
+            <slide v-else v-for="company in companies" :key="company">
+              <a target="_blank" :href="company.link ? company.link : 'jeec.ist'" class="carousel__item">
+                <img class="company__logo_solo" :src="jeec_api_url + company.logo" :alt="company.name">
+              </a>
+            </slide>
+          </div>
+        </Transition>
       </div>
-    </div>
-    <div v-else class="carousel__border">
-      <div class="carousel__wrapper">
-          <slide v-for="company in companies" :key="company">
-            <a target="_blank" :href="company.link ? company.link : 'jeec.ist'" class="carousel__item">
-              <img class="company__logo_solo" :src="jeec_api_url + company.logo" :alt="company.name">
-            </a>
-          </slide>
-      </div>
+      <button class="expand-button" v-if="canExpand" @click="expanded = !expanded">
+        <p v-if="expanded">Hide</p>
+        <p v-else>Show All</p>
+      </button>
     </div>
   </div>
 </template>
@@ -55,11 +67,18 @@ export default {
     return {
       jeec_api_url: process.env.VUE_APP_JEEC_BRAIN_URL,
       imagesLoaded: false,
+      expanded: false,
+      canExpand: true,
       carousel_breakpoints: {
         0: { itemsToShow: 2.3 },
         640: { itemsToShow: 2.5 },
         1007: { itemsToShow: 2.7 }
       }
+    }
+  },
+  created() {
+    if (this.companies.length < 4) {
+      this.canExpand = false;
     }
   }
 } 
@@ -80,7 +99,7 @@ export default {
   font-size: 2.8rem;
 }
 
-.tier__title > h2 {
+.tier__title>h2 {
   position: relative;
   z-index: 2;
   text-transform: uppercase;
@@ -94,7 +113,7 @@ export default {
   padding: 0 1rem;
 }
 
-.tier > div {
+.tier>div {
   position: absolute;
   left: 50%;
   top: 50%;
@@ -105,13 +124,13 @@ export default {
   z-index: 1;
 }
 
-.tier > h2 {
+.tier>h2 {
   text-align: center;
   text-transform: uppercase;
   z-index: -1;
 }
 
-.tier svg{
+.tier svg {
   position: absolute;
   left: 0;
   top: 0;
@@ -125,7 +144,7 @@ export default {
   fill: var(--clr-dark-background);
 }
 
-.mask__text{
+.mask__text {
   text-anchor: middle;
   dominant-baseline: middle;
   alignment-baseline: central;
@@ -179,6 +198,7 @@ export default {
 #tier__text__background-bronze {
   background-color: #C9705C;
 }
+
 /* #endregion */
 
 .carousel__border {
@@ -194,6 +214,11 @@ export default {
   margin-top: 1rem;
   position: relative;
   background-color: rgb(37, 37, 37);
+  margin-bottom: 2rem;
+}
+
+.carousel__border.expanded {
+  height: auto;
 }
 
 .carousel__wrapper {
@@ -205,6 +230,41 @@ export default {
   border-radius: 9px;
   overflow: hidden;
   background: rgb(61, 61, 61);
+
+  max-height: calc(100% - 4px);
+  transition: all 0.5s ease-in-out;
+  transition-delay: 0;
+}
+
+.carousel__border.expanded .carousel__wrapper {
+  max-height: 5000px;
+  transition: all 0.5s ease-in-out;
+  transition-delay: 0.5s;
+}
+
+.fade-grow-enter-active,
+.fade-grow-leave-active {
+  transition: all 0.5s ease-in-out;
+}
+
+.fade-grow-leave-active {
+  transition-duration: 0s;
+}
+
+.fade-grow-enter-from,
+.fade-grow-leave-to {
+  opacity: 0;
+}
+
+.expanded-wrapper {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.expanded-wrapper .carousel__slide {
+  width: auto;
+  flex-basis: 33%;
 }
 
 .carousel__slide {
@@ -236,23 +296,40 @@ export default {
   height: 90%;
   object-fit: contain;
 }
+
 .company__logo_solo {
   width: 70%;
   height: 70%;
   object-fit: contain;
 }
 
+.expand-button {
+  position: absolute;
+  top: calc(100% + 0.2rem);
+  right: 0.3rem;
+  background: none;
+  border: none;
+  cursor: pointer;
+  border: 2px solid var(--clr-dark-font);
+  padding: 0.3rem 1ch;
+  border-radius: 20px;
+  height: 1.8rem;
+}
+
 @media screen and (max-width: 1007px) {
   .mask__text {
     font-size: 2.6rem;
   }
+
   .tier__title h2 {
     margin-top: -0.1rem;
     font-size: 2.6rem;
   }
+
   .carousel__border {
     width: 80%;
   }
+
   .carousel__slide {
     padding: 10px 8px;
   }
@@ -280,4 +357,5 @@ export default {
   .carousel__item {
     height: 140px;
   }
-}</style>
+}
+</style>
